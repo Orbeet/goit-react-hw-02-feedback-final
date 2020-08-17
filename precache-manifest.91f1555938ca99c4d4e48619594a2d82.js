d19a6b099eac9a31512d5330fc115ae234a53d00
orbeet <excenter8@gmail.com>
2020-08-17 13:36:16 +0300

    Updates

---
+++ precache-manifest.91f1555938ca99c4d4e48619594a2d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b9a147e6bcecadd0bea82d3ed3c645",
+    "url": "/goit-react-hw-02-feedback-final/index.html"
+  },
+  {
+    "revision": "9c84b1dea07722d62195",
+    "url": "/goit-react-hw-02-feedback-final/static/css/main.da93ae35.chunk.css"
+  },
+  {
+    "revision": "29b89ff345a5177a4ced",
+    "url": "/goit-react-hw-02-feedback-final/static/js/2.2ed98505.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback-final/static/js/2.2ed98505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c84b1dea07722d62195",
+    "url": "/goit-react-hw-02-feedback-final/static/js/main.2d9743fa.chunk.js"
+  },
+  {
+    "revision": "cc5ea32085187e3558d4",
+    "url": "/goit-react-hw-02-feedback-final/static/js/runtime-main.04cc1042.js"
+  }
+]);
